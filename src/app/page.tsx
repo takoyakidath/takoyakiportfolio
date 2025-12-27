ae3328f922e6690a9c616498e1b8f09991e631df
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-12-27 18:03:14 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,17 @@ import { ChevronDown } from 'lucide-react';
 export default function Home() {
   return (
     <div>
-
-<div>
-<div className="flex flex-col items-center justify-center h-screen">
-        <Image src="/takoyaki.png" alt="Takoyaki Icon" width={200} height={200} />
-        <div className="text-base md:text-xl lg:text-2xl font-bold">Software Engineer</div>
-        <div className="text-4xl md:text-6xl lg:text-8xl font-bold">Ryunosuke Yoda</div>
+      <div className="relative h-screen">
+        <div className="flex flex-col items-center justify-center h-screen">
+          <Image src="/takoyaki.png" alt="Takoyaki Icon" width={200} height={200} />
+          <div className="text-base md:text-xl lg:text-2xl font-bold">Software Engineer</div>
+          <div className="text-4xl md:text-6xl lg:text-8xl font-bold">Ryunosuke Yoda</div>
+        </div>
+        <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center justify-center font-bold">
+          <span className="text-xs">Scroll down</span>
+          <ChevronDown className="animate-bounce" />
+        </div>
       </div>
-      <div className="flex flex-col items-center justify-center font-bold">
-      <span className="text-xs">Scroll down</span>
-      <ChevronDown className="animate-bounce" />
-      </div>
-</div>
       <div>
         About me
       </div>
