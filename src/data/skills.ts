ca090e7be23cf6bfac24dc885a6cd386551dd0ea
TakoyakiDayo <Takoyaki1051@outlook.jp>
2025-12-28 11:04:52 +0900

    fix data

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -4,11 +4,11 @@ export interface Skill {
 }
 
 export const skills: Skill[] = [
-  { name: 'Linux', percentage: 0.85 },
-  { name: 'TypeScript', percentage: 0.80 },
-  { name: 'Next.js', percentage: 0.75 },
-  { name: 'Kubernetes', percentage: 0.70 },
-  { name: 'Python', percentage: 0.65 },
-  { name: 'React Native', percentage: 0.90 },
+  { name: 'Linux', percentage: 0.90 },
+  { name: 'TypeScript', percentage: 0.85 },
+  { name: 'Next.js', percentage: 0.80 },
+  { name: 'Kubernetes', percentage: 0.50 },
+  { name: 'Python', percentage: 0.30 },
+  { name: 'React Native', percentage: 0.50 },
 ];
 
