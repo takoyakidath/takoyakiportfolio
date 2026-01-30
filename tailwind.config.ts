ff2e03a6f7492ddb0232383c35f4e466c1070b0d
takoyakidath <takoyaki1051@outlook.jp>
2026-01-30 22:18:43 +0900

    update

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from "tailwindcss"
+
+const config: Config = {
+  theme: {
+    extend: {
+      fontFamily: {
+        "mochiy-pop": ['"Mochiy Pop One"', "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+}
+
+export default config
